6157081b5c9db732c25cc5ed6e8e16abcebf3485
Nutter Fi <itsnutterfi@gmail.com>
2022-10-25 20:13:18 -0700

    Initial Commit

---
+++ CistercianNumbers/CistercianNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CistercianNumbersApp.swift
+//  CistercianNumbers
+//
+//  Created by nutterfi on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CistercianNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
